aa359f192ea672a3c27013b06ccfe617023d7786
dinom <dinomc222@mail.ru>
2023-06-02 20:15:21 +0300

    Some message

--- SimpleJavaProject/src/main/java/denis_grimaliuc/Main.java
+++ SimpleJavaProject/src/main/java/denis_grimaliuc/Main.java
@@ -5,7 +5,7 @@ public class Main {
 
 
     public static void main(String[] args) {
-        String str = "QWEASDSAFAASFQWRRQW";
+        String str = "QWEASDSA FAASFQWRRQW";
         System.out.printf(str + "\n\n\n\n\n");
     }
 
