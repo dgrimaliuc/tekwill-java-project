b10943f9b38de5a91bbf77c6876a726cecae4d14
dinom <dinomc222@mail.ru>
2023-06-02 20:11:56 +0300

    Initial Project

---
+++ SimpleJavaProject/src/main/java/denis_grimaliuc/Main.java
@@ -0,0 +1,13 @@
+package denis_grimaliuc;
+
+
+public class Main {
+
+
+    public static void main(String[] args) {
+        String str = "QWEASDSAFAASFQWRRQW";
+        System.out.printf(str + "\n\n\n\n\n");
+    }
+
+
+}
