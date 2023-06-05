0e87c489098ea95d178d6f76cbe99b9295d41281
CristinaSavrin <135253784+CristinaSavrin@users.noreply.github.com>
2023-06-05 22:31:23 +0300

    Initial Project (#3)
    Merging

---
+++ SimpleJavaProject/src/main/java/cristina_savrin/Main.java
@@ -0,0 +1,14 @@
+package cristina_savrin;
+
+public class Main {
+
+    public static void main(String[] args) {
+        
+        int x = 32, y = 8;
+        System.out.println("x+y=" + (x + y));
+        System.out.println("x-y=" + (x - y));
+        System.out.println("x*y=" + (x * y));
+        System.out.println("x/y=" + (x / y));
+    }
+
+}
