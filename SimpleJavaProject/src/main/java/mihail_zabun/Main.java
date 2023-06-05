3d5815e2f63a966f5372a792d1b990ca60685c85
Mihail Zabun <zabunmihail@gmail.com>
2023-06-05 16:18:03 +0300

    mihail zabun first java task

---
+++ SimpleJavaProject/src/main/java/mihail_zabun/Main.java
@@ -0,0 +1,13 @@
+package mihail_zabun;
+
+public class Main {
+    public static void main(String[] args) {
+        int a = 1500;
+        int b = 5;
+        System.out.println(a-b);
+        System.out.println(a+b);
+        System.out.println(a*b);
+        System.out.println(a/b);
+
+    }
+}
