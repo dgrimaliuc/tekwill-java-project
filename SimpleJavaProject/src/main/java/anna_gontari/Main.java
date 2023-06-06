25e3001de552da0724e26970ef234e90520c22eb
Gont-gont <a.gontari10@gmail.com>
2023-06-06 23:12:32 +0300

    some message

---
+++ SimpleJavaProject/src/main/java/anna_gontari/Main.java
@@ -0,0 +1,14 @@
+package anna_gontari;
+
+public class Main {
+    public static void main(String[] args) {
+        int c = 5;
+        int b = 15;
+        int a = 3;
+        System.out.println(c + b);
+        System.out.println(b - a);
+        System.out.println(b / c);
+        System.out.println(a * b);
+
+    }
+}
