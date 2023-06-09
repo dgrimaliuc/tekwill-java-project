c0af1967c2c435e31162b391145022ccb043f776
Roman <roman.marco@gmail.com>
2023-06-09 23:28:02 +0300

    NumbersMain class

--- SimpleJavaProject/src/main/java/roman_marcov/NumbersMain.java
+++ SimpleJavaProject/src/main/java/roman_marcov/NumbersMain.java
@@ -6,30 +6,34 @@ import java.util.Scanner;
 
 public class NumbersMain {
     public static void main(String[] args) {
-  /*
-        Numbers:
-1. Find the square root of a number.
-2. Generate a random number between 0 and 100. (Try the same between 10 and 100)
-3. Check if a number is prime.
-4. Calculate the average of three numbers.
-5. Find the maximum of 4 numbers.
-  */
-    //#1
-       /* Scanner scan = new Scanner(System.in);
+        SquareNumbers();
+        BetweenNumbers();
+        PrimeNumber();
+        AverageNumber();
+        MaximumNumbers();
+    }
+
+    //#1. Find the square root of a number.
+    public static void SquareNumbers() {
+        Scanner scan = new Scanner(System.in);
         System.out.println("Enter number:");
         int x = scan.nextInt();
         double r = Math.sqrt(x);
         System.out.println("The square root of " + x + " is " + r);
-    */
-    //#2
-        /*Random num = new Random();
+    }
+
+    //#2. Generate a random number between 0 and 100. (Try the same between 10 and 100)
+    public static void BetweenNumbers() {
+        Random num = new Random();
         int low = 10;
         int high = 100;
-        int result = num.nextInt(high-low) + low;
+        int result = num.nextInt(high - low) + low;
         System.out.println("Random number is: " + result);
-        */
-    //#3
-    /*    Scanner scan = new Scanner(System.in);
+    }
+
+    //#3. Check if a number is prime.
+    public static void PrimeNumber() {
+        Scanner scan = new Scanner(System.in);
         System.out.println("Enter number:");
         int k = scan.nextInt();
         boolean isPrime = true;
@@ -43,11 +47,13 @@ public class NumbersMain {
             System.out.println("Number " + k + " is prime");
         } else {
             System.out.println("Number " + k + " is NOT prime");
-        }*/
+        }
+    }
 
-    //#4
+    //#4. Calculate the average of three numbers.
+    public static void AverageNumber() {
         System.out.println("Enter 3 numbers: ");
-        Scanner input = new Scanner (System.in);
+        Scanner input = new Scanner(System.in);
         DecimalFormat decimalFormat = new DecimalFormat("0.00##");
         int num1;
         int num2;
@@ -56,7 +62,19 @@ public class NumbersMain {
         num1 = input.nextInt();
         num2 = input.nextInt();
         num3 = input.nextInt();
-        avg = (double)(num1 + num2 + num3) / 3.0;
+        avg = (double) (num1 + num2 + num3) / 3.0;
         System.out.println("The average is: " + decimalFormat.format(avg));
+    }
+
+
+    //#5. Find the maximum of 4 numbers.
+    public static void MaximumNumbers() {
+        int arr[] = {1, 3, 45, 22, 10};
+        int maxValue = Integer.MIN_VALUE;
+
+        for (int i = 0; i < arr.length; ++i) {
+            maxValue = Math.max(maxValue, arr[i]);
+        }
+        System.out.println("Max number is: " + maxValue);
     }
 }
