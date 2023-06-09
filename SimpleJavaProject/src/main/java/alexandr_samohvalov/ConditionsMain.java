cbb7639d8b3a383d4c30fefc03573d551b7c7d79
Alex <samohvalov.alexx@gmail.com>
2023-06-09 19:00:36 +0300

    Checking git push

---
+++ SimpleJavaProject/src/main/java/alexandr_samohvalov/ConditionsMain.java
@@ -0,0 +1,19 @@
+package alexandr_samohvalov;
+
+import java.util.Scanner;
+
+public class ConditionsMain {
+    // Check if a number is positive, negative, or zero.
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        if (a > 0) {
+            System.out.println("Number is positive");
+        } else if (a < 0) {
+            System.out.println("Number is negative");
+        }else {
+            System.out.println("Number is equal to zero");
+        };
+    }
+}
+
