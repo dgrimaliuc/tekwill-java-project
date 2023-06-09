ca88b6ce7c4743f8f3b349c0eb507a01be1f5a07
Roman <roman.marco@gmail.com>
2023-06-09 21:21:37 +0300

    first java task

---
+++ SimpleJavaProject/src/main/java/roman_marcov/Main.java
@@ -0,0 +1,13 @@
+package roman_marcov;
+
+public class Main {
+    public static void main(String[] args) {
+    int x = 20;
+    int y = 8;
+        System.out.println("summ is:           " + (x + y));
+        System.out.println("difference is:     " + (x - y));
+        System.out.println("multiplication  is:" + (x * y));
+        System.out.println("devision  is:      " + (x / y));
+    }
+
+}
