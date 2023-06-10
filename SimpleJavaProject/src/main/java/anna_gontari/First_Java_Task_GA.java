ab0b7e6b483d4f4067c25b6a7d4997af3d38516b
Gont-gont <a.gontari10@gmail.com>
2023-06-10 22:58:59 +0300

    first_java-task_GA

---
+++ SimpleJavaProject/src/main/java/anna_gontari/First_Java_Task_GA.java
@@ -0,0 +1,14 @@
+package anna_gontari;
+
+public class First_Java_Task_GA {
+    public static void main(String[] args) {
+    int a = 5;
+    int b = 20;
+    int c = 4;
+    int d = 2;
+        System.out.println(a + b);
+        System.out.println(b - c);
+        System.out.println(c * a);
+        System.out.println(b / d);
+    }
+}
