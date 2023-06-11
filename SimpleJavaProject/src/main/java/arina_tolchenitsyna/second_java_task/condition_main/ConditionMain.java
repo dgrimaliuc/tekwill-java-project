90c644639a6e014a73d025cc59ec0fd2de5944db
Arina Tolchenitsyna <293173@via.dk>
2023-06-11 21:04:32 +0200

    Second Java task done.

--- SimpleJavaProject/src/main/java/arina_tolchenitsyna/second_java_task/condition_main/ConditionMain.java
+++ SimpleJavaProject/src/main/java/arina_tolchenitsyna/second_java_task/condition_main/ConditionMain.java
@@ -16,16 +16,23 @@ public class ConditionMain {
         }
         int score = 100;
         String grade;
-        if (score >= 90) {
-            grade = "A";
-        } else if (score >= 80) {
-            grade = "B";
-        } else if (score >= 70) {
-            grade = "C";
-        } else if (score >= 60) {
-            grade = "D";
-        } else {
-            grade = "F";
+        switch (score / 10) {
+            case 10:
+            case 9:
+                grade = "A";
+                break;
+            case 8:
+                grade = "B";
+                break;
+            case 7:
+                grade = "C";
+                break;
+            case 6:
+                grade = "D";
+                break;
+            default:
+                grade = "F";
+                break;
         }
         System.out.println("Your grade is " + grade);
     }
