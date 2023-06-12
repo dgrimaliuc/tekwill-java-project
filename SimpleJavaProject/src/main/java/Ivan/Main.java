f85b3da73c62ba11508ae155aed01cce0e9acd79
Ivn <neulovimiivanea@gmail.com>
2023-06-12 21:00:39 +0300

    Maiin class created

---
+++ SimpleJavaProject/src/main/java/Ivan/Main.java
@@ -0,0 +1,15 @@
+package Ivan;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 4;
+
+        System.out.println(a + b);
+        System.out.println(a - b);
+        System.out.println(a * b);
+        System.out.println(a / b);
+    }
+
+}
