bb2a1512d268f5a868644d05a2b668e9835135cd
Dima12A <qadmitriitest@gmail.com>
2023-06-12 20:44:02 +0300

    created main

---
+++ SimpleJavaProject/src/main/java/tofan_dmitrii/Main.java
@@ -0,0 +1,13 @@
+package tofan_dmitrii;
+
+public class Main {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 28;
+        System.out.println(a - b);
+        System.out.println(a + b);
+        System.out.println(a * b);
+        System.out.println((float) a / b);
+
+    }
+}
