dba4a222b6d7840e36233abf4388ff780e6c7e14
Mihail Zabun <zabunmihail@gmail.com>
2023-06-13 08:15:35 +0300

    1 and 2 Java Homework

---
+++ SimpleJavaProject/src/main/java/zabun_mihail/Main.java
@@ -0,0 +1,13 @@
+package zabun_mihail;
+
+public class Main {
+    public static void main(String[] args) {
+        int a = 1500;
+        int b = 5;
+        System.out.println(a-b);
+        System.out.println(a+b);
+        System.out.println(a*b);
+        System.out.println(a/b);
+
+    }
+}
