f15f2d42165bc2aa7f62d8ab9ef07bdf3a9de4b5
Oleg <tabanskii22@gmail.com>
2023-06-15 00:13:30 +0300

    Some message

---
+++ SimpleJavaProject/src/main/java/oleg_tabanski/Main.java
@@ -0,0 +1,12 @@
+package oleg_tabanski;
+
+public class Main {
+    public static void main(String[] args) {
+        int a = 8;
+        int b = 3;
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a*b);
+        System.out.println(a/b);
+    }
+}
