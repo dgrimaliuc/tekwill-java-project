82fcb9773539db05f09119129c5e93d7460dcd3b
LiudaL <liudalyash@gmail.com>
2023-06-14 14:15:28 -0400

    Task # 4

--- src/main/java/Leas_Liudmila/Map.java
+++ src/main/java/Leas_Liudmila/Map.java
@@ -6,7 +6,15 @@ public class Map {
 
     //1. Create a Map that stores the abbreviation as keys and names of countries as values. Add some pairs to the map and retrieve the capital of a specific country.
     public static void main(String[] args) {
-        HashMap<String, String> countryCodes = new HashMap<>();
+        java.util.Map<String, String[]> countries = new HashMap<>();
+        countries.put("MD", new String[]{"Moldova", "Chisinau"});
+        countries.put ("RO", new String[]{"Romania", "Bucharest"});
+        countries.put ("USA", new String[]{"America", "Washington"});
+        System.out.println(countries.get("USA")[1]);
+
+
+
+       /* HashMap<String, String> countryCodes = new HashMap<>();
         HashMap<String, String> countryCapitals = new HashMap<>();
         countryCapitals.put("Romania", "Bucharest");
         countryCapitals.put("Moldova", "Chisinau");
@@ -16,7 +24,7 @@ public class Map {
 
         String moldovaCapital = countryCapitals.get(countryCodes.get("MD"));
 
-        System.out.println(moldovaCapital);
+        System.out.println(moldovaCapital); */
     }
 
 
