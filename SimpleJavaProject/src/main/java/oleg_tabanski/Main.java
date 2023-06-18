fb1f4bd97b2b1f98b4a4c439840ebcd48edd4e83
Oleg <tabanskii22@gmail.com>
2023-06-18 21:27:46 +0300

    Java task

--- SimpleJavaProject/src/main/java/oleg_tabanski/Main.java
+++ SimpleJavaProject/src/main/java/oleg_tabanski/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         int a = 8;
         int b = 3;
-        System.out.println(a+b);
+        System.out.println();
         System.out.println(a-b);
         System.out.println(a*b);
         System.out.println(a/b);
