39c774d05c93c156aaf62ee956f92079d6937926
CristinaSavrin <kristinkap@gmail.com>
2023-06-19 13:04:39 +0300

    CristinaSavrin Fifth Java Task (OOP)

---
+++ src/main/java/cristina_savrin/oop/inheritance/Shape.java
@@ -0,0 +1,21 @@
+package cristina_savrin.oop.inheritance;
+
+/**
+ * Create a class named "Shape" with instance variables for color.
+ */
+
+public class Shape {
+    private String color;
+
+    public Shape(String color) {
+        this.color = color;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+}
