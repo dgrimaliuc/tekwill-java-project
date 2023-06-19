5606090fa92d15b774ab38a5db76e3d7bbdf53e4
Gont-gont <a.gontari10@gmail.com>
2023-06-19 16:13:32 +0300

    First Java Project

--- src/main/java/anna_gontari/Main.java
+++ src/main/java/anna_gontari/Main.java
@@ -2,9 +2,9 @@ package anna_gontari;
 
 public class Main {
     public static void main(String[] args) {
-        int a = 5;
-        int b = 20;
-        int c = 4;
+        int a = 6;
+        int b = 18;
+        int c = 3;
         int d = 2;
         System.out.println(a + b);
         System.out.println(b - c);
