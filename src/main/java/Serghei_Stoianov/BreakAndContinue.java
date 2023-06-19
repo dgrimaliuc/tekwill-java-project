04d9b3a57aecd6286b6a6064d6228a6d7007a085
Serghei Stoianov <Sergey.Stoianov@gmail.com>
2023-06-19 11:02:05 +0300

    HW3

--- src/main/java/Serghei_Stoianov/BreakAndContinue.java
+++ src/main/java/Serghei_Stoianov/BreakAndContinue.java
@@ -8,7 +8,7 @@ public class BreakAndContinue {
 
 ////Create a program, which will ask user to enter a number, if number will be negative exit the loop
         Scanner scanner = new Scanner(System.in);
-        for (i = 1; i++ > 0; ) {
+        for (i = 1; i > 0; i++) {
             System.out.println("enter a number");
             int sc = scanner.nextInt();
             if (sc < 0) break;
@@ -16,8 +16,9 @@ public class BreakAndContinue {
 
 
 //Create a program which will print all number from 1 to 10 except 3 and 7 using continue keyword
-        for (i = 0; i++ < 10; ) {
-            if (i == 3 || i == 7) continue;
+        for (i = 1; i <= 10; i++) {
+            if (i == 3 || i == 7)
+                continue;
             System.out.print(i + " ");
 
         }
