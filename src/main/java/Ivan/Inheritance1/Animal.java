95ff29c0f793e42fdbd7ce565264a508efe27936
Ivn <neulovimiivanea@gmail.com>
2023-06-23 19:16:47 +0300

    Done 5.1 java task

---
+++ src/main/java/Ivan/Inheritance1/Animal.java
@@ -0,0 +1,40 @@
+package Ivan.Inheritance1;
+
+public class Animal {
+    private String name;
+    private int age;
+    private String sound;
+
+    public Animal(String name, int age, String sound) {
+        this.name = name;
+        this.age = age;
+        this.sound = sound;
+    }
+
+    // Getter and Setter methods for name
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    // Getter and Setter methods for age
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    // Getter and Setter methods for sound
+    public String getSound() {
+        return sound;
+    }
+
+    public void setSound(String sound) {
+        this.sound = sound;
+    }
+}
---
+++ src/main/java/Ivan/Inheritance1/Shape.java
@@ -0,0 +1,18 @@
+package Ivan.Inheritance1;
+
+public class Shape {
+    private String color;
+
+    public Shape(String color) {
+        this.color = color;
+    }
+
+    // Getter and Setter methods for color
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+}
