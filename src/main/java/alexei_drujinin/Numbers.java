3fdd0c040b8a83414718f6c9c842f84c6ba37698
Alexei-Dr <75645902+Alexei-Dr@users.noreply.github.com>
2023-06-26 17:09:15 +0300

    Second java task (#84)

---
+++ src/main/java/alexei_drujinin/Numbers.java
@@ -0,0 +1,32 @@
+package src.main.java.alexei_drujinin;
+
+public class Numbers {
+    static int b = (int) (Math.random() * 100);
+    static int d = (int) (Math.random() * 100);
+    static int f = (int) (Math.random() * 100);
+    static int j = (int) (Math.random() * 100);
+
+    public static void main(String[] args) {
+        squareroot(b);
+        randomNumber(d);
+        averageOfThreeNumbers();
+        maxOffourNumbers();
+    }
+
+    public static void squareroot(int a) {
+        System.out.println("The square root of " + a + " is " + Math.sqrt(a));
+    }
+
+    public static void randomNumber(int c) {
+        System.out.println("Your random number is " + c);
+    }
+
+    public static void averageOfThreeNumbers() {
+        int av = (b + d + f) / 3;
+        System.out.println("The average of " + b + " " + d + " " + f + " is " + av);
+    }
+
+    public static void maxOffourNumbers() {
+        System.out.println("The Max number of " + b + " " + d + " " + f + " " + j + "  is " + Math.max(Math.max(b, d), Math.max(f, j)));
+    }
+}
