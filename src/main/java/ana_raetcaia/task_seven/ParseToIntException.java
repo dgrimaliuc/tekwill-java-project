dff76a1dfd0d17b0e1ee11dc3e339990b2399328
Raetskaya <raetskayaann@gmail.com>
2023-06-30 01:13:20 +0300

    Change input description

--- src/main/java/ana_raetcaia/task_seven/ParseToIntException.java
+++ src/main/java/ana_raetcaia/task_seven/ParseToIntException.java
@@ -4,6 +4,6 @@ public class ParseToIntException extends RuntimeException{
     //Create personal Checked exception with name "ParseToIntException" and method which throws
     // this excecption if a string provided as parameter can not be converted to int with Integer.parseToInt() method.
     public  ParseToIntException (String input){
-        super(input);
+        super( "Can not parse int: " + input);
     }
 }
