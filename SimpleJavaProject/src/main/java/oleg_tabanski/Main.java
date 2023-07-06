d55e3bc305f73ce3ff0a066b96ccefe32a9a6ac4
Oleg <tabanskii22@gmail.com>
2023-07-06 16:54:51 +0300

    Example

--- SimpleJavaProject/src/main/java/oleg_tabanski/Main.java
+++ SimpleJavaProject/src/main/java/oleg_tabanski/Main.java
@@ -1,10 +1,11 @@
 package oleg_tabanski;
 
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         int a = 8;
         int b = 3;
-        System.out.println();
+        System.out.println(a+b);
         System.out.println(a-b);
         System.out.println(a*b);
         System.out.println(a/b);
