66194d254b873fc27f960020e4062a8531fc2b5a
Alex <samohvalov.alexx@gmail.com>
2023-07-13 10:36:29 +0300

    Sixth Java Task

--- src/main/java/alexandr_samohvalov/Interfaces/PrintMain.java
+++ src/main/java/alexandr_samohvalov/Interfaces/PrintMain.java
@@ -2,7 +2,7 @@ package alexandr_samohvalov.Interfaces;
 
 public class PrintMain {
     public static void main(String[] args) {
-        Book book1 = new Book("Java Programming", "John Smith", 39.99);
+        Book book1 = new Book("Java Prograamming", "John Smith", 39.99);
         Book book2 = new Book("Python Basics", "Jane Doe", 29.99);
         Magazine magazine1 = new Magazine("National Geographic", "National Geographic Society");
         Magazine magazine2 = new Magazine("Time", "Time USA, LLC");
