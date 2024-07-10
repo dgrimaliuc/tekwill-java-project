92ddcbeb2c9b20534fcccc25fcefbc46f6b400ac
denis.grimaliuc <denis.grimaliuc@crunchyroll.com>
2024-07-10 20:23:36 +0300

    Created first task

--- src/main/java/DenisGrimaliuc/Main.java
+++ src/main/java/DenisGrimaliuc/Main.java
@@ -1,7 +1,57 @@
 package DenisGrimaliuc;
 
+import java.util.Scanner;
+
 public class Main {
+
+    /*
+    1. Check if a number is positive, negative, or zero.
+          int number = 0;
+
+          if (number > 0){
+              System.out.println("Number is positive");
+          } else if(number < 0) {
+              System.out.println("Number is negative");
+          } else {
+              System.out.println("Number is zero");
+          }
+     2. Determine if a number is even or odd.
+            int number = 11;
+
+            if (number % 2 == 0) {
+                System.out.println("Number is even");
+            } else {
+                System.out.println("Number is odd");
+            }
+      3. Check if a number is divisible by both 2 and 3.
+             int number = 8;
+            if( number % 2 == 0 || number % 3 == 0 ){
+                System.out.println("Number is divisible by 2 or 3");
+            } else {
+                System.out.println("Number is not divisible by 2 or 3");
+            }
+
+        4.  Read a string from user and print it’s length
+             String string;
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Enter a string: ");
+            string = sc.nextLine();
+            System.out.println("Length of the string is: " + string.length());
+
+
+                String firstName;
+        String lastName;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your first name: ");
+        firstName = sc.nextLine();
+        System.out.println("Enter your last name: ");
+        lastName = sc.nextLine();
+        System.out.println("Your full name is: " + firstName + " " + lastName);
+     */
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+    String number = "1";
+
+
+
     }
 }
