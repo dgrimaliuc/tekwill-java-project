0e3b31b5a72f843fdd8b463043c5873b85461149
ulapidu <ulapidu@gmail.com>
2024-07-10 20:46:20 +0300

    This is my first Task

---
+++ src/IgorTabirta/Main.java
@@ -0,0 +1,35 @@
+package IgorTabirta;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int number = -5;
+        int x = 10;
+        int y = 5;
+        int sum = x + y;
+        System.out.println(sum);
+        int a = 3;
+        int b = 4;
+        if (a == b) {
+            System.out.println("Numbers is equal");
+        } else System.out.println("Number is't equal");
+        if (number > 0){
+            System.out.println("Number is positive");
+        } else if(number < 0) {
+            System.out.println("Number is negative");
+        } else {
+            System.out.println("Number is zero");
+        }
+        if (a > b) {
+            System.out.println("a number is MAX");
+        } else System.out.println("b number is MAX");
+        String name;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a name: ");
+        name = sc.nextLine();
+        System.out.println("Hello " + name );
+        if (a>b @@ b>x) {
+            System.out.println("a is MAX");}
+}
+
