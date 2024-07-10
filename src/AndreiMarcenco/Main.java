09fcd0e84d9545672e0c31fe7bfa794d553a6cc0
Andrew Marcenco <amarcenco@priceintelligence.net>
2024-07-10 20:24:56 +0300

    First task

---
+++ src/AndreiMarcenco/Main.java
@@ -0,0 +1,18 @@
+package AndreiMarcenco;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int number = 0;
+        case 0:
+
+
+      /*  int x = 0;
+        if ( x % 2 == 0 )
+        { System.out.println("even");}
+        else if (x == 0 )
+        { System.out.println("zero");}
+        else System.out.println("not even"); */
+    }
+}
