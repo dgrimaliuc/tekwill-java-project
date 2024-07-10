0e1c3d9536b4584439ae1e95cbd9b39478ff21cc
Victor Murashev <vmurashev@gmail.com>
2024-07-10 21:29:39 +0300

    FirstLesson
    I made some changes

--- src/main/java/VictorMurashev/Main.java
+++ src/main/java/VictorMurashev/Main.java
@@ -46,6 +46,8 @@ public class Main {
         String firstName = scanner.nextLine();
         System.out.println("Input Last Name");
         String lastName = scanner.nextLine();
+
+        
         System.out.println("Your first name is: " + firstName + "; Your last name is: " + lastName);
 
 
