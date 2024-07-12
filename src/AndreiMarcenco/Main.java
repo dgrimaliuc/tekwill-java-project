f24b456a3ff64044729534097576a7ef10bb00c5
Andrew Marcenco <andrew4242005@yahoo.com>
2024-07-12 20:45:40 +0300

    AM-task2

--- src/AndreiMarcenco/Main.java
+++ src/AndreiMarcenco/Main.java
@@ -1,18 +1,35 @@
 package AndreiMarcenco;
 
-import java.util.Scanner;
+/* public class Main {
+    public static void main(String[] args) {
+        int x = 4;
+        int y = 6;
+        int sum = x * y;
+        System.out.println(sum);
+    }
+}
 
 public class Main {
     public static void main(String[] args) {
-        int number = 0;
-        case 0:
+        int x = 4;
+        int y = 3;
+        if (x > y)
+            System.out.println(y);
+        else
+            System.out.println(x);
+    }
+}
 
+public class Main {
+    public static void main(String[] args) {
 
-      /*  int x = 0;
-        if ( x % 2 == 0 )
-        { System.out.println("even");}
-        else if (x == 0 )
-        { System.out.println("zero");}
-        else System.out.println("not even"); */
+        int age = 18;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your age");
+        age = sc.nextInt();
+        if (age >= 18)
+            System.out.println("Вы совершеннолетний");
+        else
+            System.out.println("Вы не совершеннолетний");
     }
 }
