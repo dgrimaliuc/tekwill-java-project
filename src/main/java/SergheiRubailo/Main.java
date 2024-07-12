0465283fadde1780a1d2452dce604ff6453658bc
Serghei R <61268573+serghei-rubailo@users.noreply.github.com>
2024-07-12 18:51:06 +0300

    Serghei Rubailo - Task2 #139

--- src/main/java/SergheiRubailo/Main.java
+++ src/main/java/SergheiRubailo/Main.java
@@ -8,12 +8,17 @@ public class Main {
         int x;
         int y;
         int z;
+        int temp;
+        int age;
+
         String name;
         String surname;
         int sum;
 
         Scanner scanner = new Scanner(System.in);
-
+      
+        System.out.println("Task 1!");
+      
         System.out.println("Please enter your name: ");
         name = scanner.nextLine();
 
@@ -27,12 +32,11 @@ public class Main {
 
         System.out.println("Enter Y: ");
         y = Integer.parseInt(scanner.nextLine());
-
+      
         System.out.println("Enter Z: ");
         z = Integer.parseInt(scanner.nextLine());
 
         sum = x + y;
-
         System.out.println("The sum of X and Y numbers: " + sum);
 
         if (x == y) {
@@ -42,6 +46,35 @@ public class Main {
         }
 
         System.out.println("Maximum number: " + Math.max(Math.max(x, y), z));
+
+      
+        System.out.println("Task 2!");
+        temp = x + y;
+        System.out.println("Sum: " + temp);
+
+        temp = x - y;
+        System.out.println("Diff: " + temp);
+
+        temp = x * y;
+        System.out.println("Multiplication: " + temp);
+
+        temp = x / y;
+        System.out.println("Division: " + temp);
+
+        if (x < y) {
+            System.out.println("The smallest number :" + x);
+        } else {
+            System.out.println("The smallest number :" + y);
+        }
+
+        System.out.println("Please enter your age: ");
+        age = Integer.parseInt(scanner.nextLine());
+
+        if (age >= 18) {
+            System.out.println("Вы совершеннолетний");
+        } else {
+            System.out.println("Вы не совершеннолетний");
+        }
 
     }
 }
