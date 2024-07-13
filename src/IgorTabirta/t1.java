0fcbb3ad05d8b0d7045f37b7169e54047d06d0c3
ulapidu <ulapidu@gmail.com>
2024-07-13 18:15:59 +0300

    adding Task_1

---
+++ src/IgorTabirta/t1.java
@@ -0,0 +1,10 @@
+package IgorTabirta;
+
+public class t1 {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 16;
+        int sum = a + b;
+        System.out.println(sum);
+    }
+}
