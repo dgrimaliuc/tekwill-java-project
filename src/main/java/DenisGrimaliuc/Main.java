5eb8a0cb407f87e6cea8ac0008ab131c20469ec2
Andrew Marcenco <andrew4242005@yahoo.com>
2024-07-14 10:11:49 +0300

    AM-task3

--- src/main/java/DenisGrimaliuc/Main.java
+++ src/main/java/DenisGrimaliuc/Main.java
@@ -1,7 +1,53 @@
 package DenisGrimaliuc;
 
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+
+
+
+    /*   While
+      Calculate the sum of numbers from 55 to 111 using a do-while loop.
+
+        int x = 55;
+        int y = 0;
+        while (x <= 111) {
+            x = x + 1;
+            y = x + y;
+            break; //если убрать break то увидим всю сумму чисел между int x и int y
+
+        }
+        System.out.println("sum = " + y);
+
     }
 }
+/*
+ For
+      Calculate the sum of numbers from 1 to 100 using a for loop.
+        int y = 0;
+        for (int x = 1; x <= 100; x++) {
+            {
+                y = y + x;
+            }
+            System.out.println(y);
+
+        }
+
+    }
+    }
+     */
+    /*
+              Arrays
+      Create a program that finds and displays the average value of all elements in an array of floating-point numbers.
+        float z = 0;
+        float[] x = {1, 2, 3, 4, 5, 11};
+        for (int y = 0; y < x.length; y++) {
+            z = x[y] + z;
+        }
+        System.out.println(z / x.length);  */
+
+    }
+
+}
+
+
