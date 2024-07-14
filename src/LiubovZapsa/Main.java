c8f977aa6497e442d6bdcb046a0d8d5fee59754d
Liubov <bavazapsha@gmail.com>
2024-07-14 23:14:19 +0300

    Created second task

--- src/LiubovZapsa/Main.java
+++ src/LiubovZapsa/Main.java
@@ -1,112 +1,18 @@
+public class ArithmeticOperations {
 public static void main(String[] args) {
-    package LiubovZapsa;
-
-import java.util.Scanner;
-
-    public class Main {
-        /*
-        public static void main(String[] args) {
-            int number = -4;
-            if (number > 0) {
-                System.out.println("Number is pozitive");
-            } else if (number < 0) {
-                System.out.println("Number is negative");
-            } else {
-                System.out.println("Number is zero");
-            }
-
-
-        public static void main(String[] args) {
-            int number =18;
-            if (number % 2 == 0) {
-                System.out.println("Number is even");
-            } else {
-                System.out.println("Number is add");
-        }
-
-
-
-
-    public static void main(String[] args) {
-        int number = 10;
-        if (number % 2 == 0 && number % 3 == 0) {
-            System.out.println("Number is divisible by both 2 and 3");
-        } else {
-            System.out.println("Number is not divisible by both 2 and 3");
-        }
-
-        }
-
-
-
-        public static void main(String[] args) {
-            int number = 11;
-            if (number % 2 == 0 || number % 3 == 0) {
-                System.out.println("Number is divisible by both 2 or 3");
-            } else {
-                System.out.println("Number is not divisible by both 2 or 3");
-            }
-
-
-
-
-        public static void main(String[] args) {
-            String string;
-            Scanner scanner = new Scanner(System.in);
-            System.out.println("Enter a string:m");
-            string = scanner.nextLine();
-            System.out.println("Lenght of the string is: " + string.length());
-        }
-
-
-        public static void main(String[] args) {
-            int number1 = 4;
-            int number2 = 5;
-            int sum = number1 + number2;
-            System.out.println("The sum of " + number1 + " and " + number2 + " is: " + sum);
-        }
-
-        public static void main(String[] args) {
-            int num1 = 6;
-            int num2 = 6;
-            if (num1 == num2) {
-                System.out.println("two numbers are equal");
-            } else {
-                System.out.println("two numbers are  not equal");
-            }
-        }
-
-        public static void main(String[] args) {
-            int num = -2;
-            if (num > 0) {
-                System.out.println("Number is positive");
-            } else if (num < 0) {
-                System.out.println("Number is negative");
-            } else {
-                System.out.println("Number is zero");
-            }
-        }
-
-        public static void main(String[] args) {
-            int num1 = 9;
-            int num2 = 4;
-            if (num1 > num2) {
-                System.out.println(" num1 is maximum");
-            } else {
-                System.out.println(" num2 is maximum");
-            }
-        }
-    */
-        public static void main(String[] args) {
-            String firstname;
-            String lastname;
-            Scanner sc = new Scanner(System.in);
-            System.out.println("Enter your first name");
-            firstname = sc.nextLine();
-            System.out.println("Enter your last name");
-            lastname = sc.nextLine();
-            System.out.println(" Your full name is : " + firstname + " " + lastname);
-        }
+int num1 = 20;
+int num2 = 5;
+    int sum = num1 + num2; {
+    System.out.println(summary two numbers equal);
+    int difference = num1 - num2);
+    System.out.println(difference two numbers equal);
+    int product = num1 * num2;
+        System.out.println(product of numbers equal);
+    int quotient =num1 / num2;
+        System.out.println(quotient of numbers equal);
     }
-
 }
+
+
+
+
