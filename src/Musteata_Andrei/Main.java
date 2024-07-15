29f0841de442649f07eb10158a0bf60782c12b7e
AndreiMusteata123 <andrew777mus@gmail.com>
2024-07-15 17:45:52 +0300

    variant_3

--- src/Musteata_Andrei/Main.java
+++ src/Musteata_Andrei/Main.java
@@ -4,26 +4,29 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        int x = 10;
-        int y = 2;
-        int sum = x + y;
-        int difference = x - y;
-        int multiplication = x * y;
-        int division = x / y;
-        System.out.println("sum=" + sum + "\ndifference=" + difference +
-                "\nmultiplication=" + multiplication + "\ndivision" + division);
-        if (x>y){
-            System.out.println(x);
-        }else {
-            System.out.println(y);
+        int number = 55;
+        int totalSum = 0;
+
+        do {
+            totalSum += number;
+            number++;
+        } while (number <= 111);
+        System.out.println(totalSum);
+
+        int i;
+        for (i = 1; i <= 100; i++) {
+            totalSum += i;
         }
-        Scanner scanner=new Scanner(System.in);
-        System.out.println("Enter your age");
-        int i = scanner.nextInt();
-        if (i>=18){
-            System.out.println("You're totally summery");
-        }else {
-            System.out.println("You're not totally summery");
+        System.out.println(totalSum);
+
+
+        double[] numbers = {5.5, 7.2, 3.8, 9.0, 4.6, 8.1, 6.3};
+        double sum = 0.0;
+        for (int a = 0; a < numbers.length; a++) {
+            sum += numbers[a];
         }
+        System.out.println(sum);
+        double average = sum / numbers.length;
+        System.out.println("The average value of all elements in the array is: " + average);
     }
 }
