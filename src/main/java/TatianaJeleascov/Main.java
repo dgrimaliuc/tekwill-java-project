93a465e63436d7d3b2890c3d5383d7d9d17440d3
InCreating <123948791+InCreating@users.noreply.github.com>
2024-07-15 11:42:01 +0300

    Third homework with 3 tasks (#165)

--- src/main/java/TatianaJeleascov/Main.java
+++ src/main/java/TatianaJeleascov/Main.java
@@ -17,6 +17,10 @@ public class Main {
         System.out.println("Enter your age: ");
         int age = Integer.parseInt(scanner.nextLine());
         System.out.println(ageIdentifier(age));
+        
+        doWhileCycle();
+        forCycle();
+        arraysAvg();
     }
 
     private static void calculator(int numOne, int numTwo) {
@@ -37,6 +41,34 @@ public class Main {
     private static String ageIdentifier(int age) {
         return (age > 18) ? "Вы совершеннолетний" : "Вы не совершеннолетний";
     }
+    
+    private static void doWhileCycle()  {
+        int temp = 0;
+        int i = 55;
+        do {
+            temp += i;
+            i++;
+        } while (i <= 111);
+        System.out.println("Task 1 is: " + temp);
+    }
+    
+    public static void forCycle() {
+        int temp = 0;
+        for(int i=1; i<=100; i++) {
+            temp+=i;
+        }
+        System.out.println("Task 2 is: " + temp);
+    }
+    
+    
+    public static void arraysAvg() {
+    double[] arr = {1, 2, 3, 4, 5}; 
+    double temp = 0;
+    
+    for (int i = 0; i < arr.length; i++) { 
+        temp += arr[i];
+    }
+    
+    System.out.println("Task 3 is: " + (temp / arr.length)); 
+    }
 }
-
-
