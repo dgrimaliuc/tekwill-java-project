b87edb7d336350382771f40097ded5fb351dc8b6
angoncear <anastasia.goncear@endava.com>
2024-07-15 11:00:33 +0300

    Lesson3HomeWork Anastasia Goncear

---
+++ src/main/java/Lesson3AnastasiaGoncear/Scanner.java
@@ -0,0 +1,16 @@
+package Lesson3AnastasiaGoncear;
+
+import java.io.InputStream;
+
+public class Scanner {
+    public Scanner(InputStream in) {
+
+    }
+
+    public int nextInt() {
+        return 0;
+    }
+
+    public void close() {
+    }
+}
