71224f502908d8f7569c60e9f459a7fcb6eea0a4
Chiril Bortnicov <td1900000@gmail.com>
2024-07-17 16:58:06 +0300

    src/ChirilBortnicov/Task2.java

---
+++ src/ChirilBortnicov/Task2.java
@@ -0,0 +1,33 @@
+package ChirilBortnicov;
+
+import java.util.Scanner;
+
+public class Task2 {
+    /* public static void main(String[] args) {
+        int a = 22;
+        int b = 10;
+        System.out.println("Sum of numbers: " + (a + b));
+        System.out.println("Number difference: " + (a - b));
+        System.out.println("Product of numbers: " +(a * b));
+        System.out.println("Dividing numbers: " +(a / b));
+    } */
+  /* public static void main(String[] args) {
+       int a = 50;
+       double b = 49.9;
+       if (a < b){
+           System.out.println("First number is less");
+       } else {
+           System.out.println("Second number is less");
+       }
+   } */
+    public static void main(String[] args) {
+       System.out.println("Insert your age: ");
+       Scanner sc = new Scanner(System.in);
+       int age = sc.nextInt();
+       if (age >= 18){
+           System.out.println("Вы совершеннолетний");
+       } else {
+           System.out.println("Вы не совершеннолетний");
+       }
+   }
+}
