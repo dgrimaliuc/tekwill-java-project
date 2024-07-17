71900f27ec451b4c580b59cfd9e990663da04303
AnastasiaGoncear <152273674+AnastasiaGoncear@users.noreply.github.com>
2024-07-17 18:38:24 +0300

    Merge remote-tracking branch 'origin/Lesson3HomeWorkAnastasiaGoncear' into Lesson3HomeWorkAnastasiaGoncear (#185)
    Co-authored-by: angoncear <anastasia.goncear@endava.com>

---
+++ src/main/java/Lesson4HomeWorkAnastasiaGoncear/Task1ArrayList.java
@@ -0,0 +1,17 @@
+package Lesson4HomeWorkAnastasiaGoncear;
+//Create an array ArrayList of strings and add some fruits inside, print array list on screen
+import java.util.ArrayList;
+
+public class Task1ArrayList {
+    public static void main(String[] args) {
+        ArrayList<String> fruits = new ArrayList<>();
+        fruits.add("apple");
+        fruits.add("banana");
+        fruits.add("peach");
+        fruits.add("plum");
+        fruits.add("cherry");
+
+        for (int i = 0; i < fruits.size(); i++) {
+            System.out.println(fruits.get(i));
+        }
+}}
