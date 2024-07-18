c76275d2d83aff782ca0b813f7e3de5af7d96d83
TitchievI <inga.titchiev@math.md>
2024-07-18 20:33:41 +0300

    Home work Titchiev 12.07

---
+++ src/main/java/IngaTitchiev/_HW_17_07/Animals.java
@@ -0,0 +1,26 @@
+package IngaTitchiev._HW_17_07;
+
+public class Animals {
+    private String name;
+    private String type;
+    private int age;
+    private String sound;
+
+   public Animals(String name, String type, int age, String sound) {
+        this.name = name;
+        this.type = type;
+        this.age = age;
+        this.sound = sound;
+    }
+
+    public void makeSound() {
+        System.out.println(sound);
+    }
+
+     public void displayInfo() {
+        System.out.println("Name: " + name);
+        System.out.println("Type: " + type);
+        System.out.println("Age: " + age);
+    }
+
+}
