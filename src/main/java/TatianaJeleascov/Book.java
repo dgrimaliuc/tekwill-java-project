1a08195a27f562152f256d9acd9800bc5191daee
InCreating <123948791+InCreating@users.noreply.github.com>
2024-07-19 13:10:30 +0300

    Created Book class

---
+++ src/main/java/TatianaJeleascov/Book.java
@@ -0,0 +1,58 @@
+public class Book {
+    private String title;
+    private String author;
+    private double price;
+    private String isbn;
+
+    public Book(String title, String author, double price, String isbn) {
+        this.title = title;
+        this.author = author;
+        this.price = price;
+        this.isbn = isbn;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public void displayDetails() {
+        System.out.println("Title: " + title);
+        System.out.println("Author: " + author);
+        System.out.println("Price: " + price);
+        System.out.println("ISBN: " + isbn);
+    }
+
+    public void applyDiscount(double percentage) {
+        double discountAmount = price * (percentage / 100);
+        double discountedPrice = price - discountAmount;
+        System.out.println("Discounted Price after " + percentage + "% off: $" + discountedPrice);
+    }
+}
