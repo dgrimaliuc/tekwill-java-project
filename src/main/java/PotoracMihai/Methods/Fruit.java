43436dc64c72be746ef560174d1477c698d7f8cb
Mihai <potoracmihai74@gmail.com>
2024-07-19 18:32:14 +0300

    PM-task5

---
+++ src/main/java/PotoracMihai/Methods/Fruit.java
@@ -0,0 +1,15 @@
+package main.java.PotoracMihai.Methods;
+
+public class Fruit {
+
+    public String name;
+
+    public String color;
+    public int calories;
+
+    public Fruit(String name, String color, int calories) {
+        this.name = name;
+        this.color = color;
+        this.calories = calories;
+    }
+}
