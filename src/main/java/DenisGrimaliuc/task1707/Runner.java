be8ac960768915d58fba7304ba0eccdb9078390f
denis.grimaliuc <denis.grimaliuc@crunchyroll.com>
2024-07-19 21:00:08 +0300

    Added DG classes Fixed error

---
+++ src/main/java/DenisGrimaliuc/task1707/Runner.java
@@ -0,0 +1,11 @@
+package DenisGrimaliuc.task1707;
+
+public class Runner {
+
+    public static void main(String[] args) {
+
+        // Create a class called Book with fields such as title, author, year and price.
+        Book book = new Book("The Lord of the Rings", "J.R.R. Tolkien", 1954, 10.5);
+        System.out.println("Title: " + book.title);
+    }
+}
