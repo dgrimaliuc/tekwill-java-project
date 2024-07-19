fdcc672de4493a9e3d9e8aa5a86a5affbe2cd943
Kopati47 <alexandr.mc1@gmail.com>
2024-07-19 12:02:38 +0300

    17.07.2024

--- src/TatianaDolghii/Main.java
+++ src/TatianaDolghii/Main.java
@@ -2,7 +2,18 @@ package TatianaDolghii;
 
 public class Main {
 
-    public static void main(String[] args) {
-        System.out.println("Hello World");
+    public class Fruit {
+        // Create a class named Fruit with instance variables such as name, color and calories..
+        String name;
+        String colors;
+        int calories;
+
+
+        Fruit(String name, String colors, int calories) {
+            this.name = name;
+            this.name = author;
+            this.year = year;
+            this.price = price;
+        }
     }
 }
