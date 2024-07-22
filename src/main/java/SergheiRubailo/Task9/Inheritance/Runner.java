1b063e343a346d3001e0a50d3d08ebaf9c0ec9d1
serghei-rubailo <serghei.rubailo@gmail.com>
2024-07-22 18:14:24 +0300

    Remove files from another branch

--- src/main/java/SergheiRubailo/Task9/Inheritance/Runner.java
+++
@@ -1,9 +0,0 @@
-package SergheiRubailo.Task9.Inheritance;
-
-public class Runner {
-    public static void main(String[] args) {
-        Student student = new Student("John", 25, "Seaseme Street", 23, "IT");
-
-        student.showInfo();
-    }
-}
