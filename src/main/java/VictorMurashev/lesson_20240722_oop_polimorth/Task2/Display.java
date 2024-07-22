71a8e9610f1279f5f0031bb6e67a7d6efd589639
Victor Murashev <vmurashev@gmail.com>
2024-07-22 19:40:15 +0300

    VictorMurashev-20240722-oop-display-diff_type

---
+++ src/main/java/VictorMurashev/lesson_20240722_oop_polimorth/Task2/Display.java
@@ -0,0 +1,18 @@
+package VictorMurashev.lesson_20240722_oop_polimorth.Task2;
+
+public class Display {
+    // display an integer value
+    public void show(int value) {
+        System.out.println("Displaying integer: " + value);
+    }
+
+    // display a double value
+    public void show(double value) {
+        System.out.println("Displaying double: " + value);
+    }
+
+    // display a String value
+    public void show(String value) {
+        System.out.println("Displaying string: " + value);
+    }
+}
