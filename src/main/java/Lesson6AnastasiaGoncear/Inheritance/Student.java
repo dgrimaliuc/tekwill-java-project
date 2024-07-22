530e4bf49f55eaf2a139e15ceb79d05aa094fa68
angoncear <anastasia.goncear@endava.com>
2024-07-22 11:58:37 +0300

    Merge remote-tracking branch 'origin/Lesson3HomeWorkAnastasiaGoncear' into Lesson3HomeWorkAnastasiaGoncear

---
+++ src/main/java/Lesson6AnastasiaGoncear/Inheritance/Student.java
@@ -0,0 +1,21 @@
+package Lesson6AnastasiaGoncear.Inheritance;
+
+public class Student extends Person {
+
+    private String studentID;
+    private String grade;
+
+    public Student(String name, int age, String address, String studentID, String grade) {
+        super(name, age, address);
+        this.studentID = studentID;
+        this.grade = grade;
+    }
+   public void printInfo(){
+       System.out.println("Name "+ getName());
+       System.out.println("Age " + getAge());
+       System.out.println("Address " + getAddress());
+       System.out.println("StudentID "+studentID);
+       System.out.println("Grade "+grade);
+    }
+
+}
