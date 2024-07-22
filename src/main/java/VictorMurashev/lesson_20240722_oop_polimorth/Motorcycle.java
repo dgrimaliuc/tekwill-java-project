1d285f9be80adc75310f8e0d8a2dd7be78b43c68
Victor Murashev <vmurashev@gmail.com>
2024-07-22 19:22:11 +0300

    VictorMurashev-20240722-oop-vehicle-sounds

---
+++ src/main/java/VictorMurashev/lesson_20240722_oop_polimorth/Motorcycle.java
@@ -0,0 +1,8 @@
+package VictorMurashev.lesson_20240722_oop_polimorth;
+
+public class Motorcycle extends Vehicle {
+    @Override
+    public void startEngine() {
+        System.out.println("The motorcycle's engine starts with brap-brap-brap.");
+    }
+}
