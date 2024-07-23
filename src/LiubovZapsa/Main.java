4a7ce5cc51d2b1bbef5c1e2f8a41f74b77bfbaa7
Liubov <bavazapsha@gmail.com>
2024-07-23 17:22:14 +0300

    Created Task13.1

--- src/LiubovZapsa/Main.java
+++ src/LiubovZapsa/Main.java
@@ -1,44 +1,19 @@
 package LiubovZapsa;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.LinkedList;
+
 public class Main {
 
     public static void main(String[] args) {
-        /* Создайте две переменные и найдите их сумму (+),
-        разность (-), произведение (*) и частное (/) и выведете
-         каждый результат на экран
-        int num1 = 20;
-        int num2 = 5;
-        int sum = num1 + num2;
-        System.out.println("summary two numbers equal: " + sum);
-        int difference = num1 - num2;
-        System.out.println("difference two numbers equal:" + difference);
-        int product = num1 * num2;
-        System.out.println("product of numbers equal: " + product);
-        int quotient = num1 / num2;
-        System.out.println("quotient of numbers equal: " + quotient);
-    }
-}
-        //Выведите на экран наименьшее из двух чисел
-        int num1 = 64;
-        int num2 = 14;
-        if (num1 > num2) {
-            System.out.println("Min number:" + num2);
-        } else {
-            System.out.println("Min number:" + num1);
-        }
-    }
-}
-    // С помощью Scanner попросите пользователя ввести
-        // свой возраст, если число больше либо равно
-        // 18 выведите “Вы совершеннолетний” иначе “Вы
-        // не совершеннолетний”
- */
-        int age = 18;
-        if (age >= 18) {
-            System.out.println("Вы совершеннолетний");
-        } else {
-            System.out.println("Вы не совершеннолетний");
-        }
+        LinkedList<String> linkedlist = new LinkedList<>(Arrays.asList("My", "curs", "is", "Java"));
+        System.out.println("Before sorting:" + linkedlist);
+        Collections.sort(linkedlist);
+        System.out.println("After sorting:" + linkedlist);
+        Collections.shuffle(linkedlist);
+        System.out.println("After sorting:" + linkedlist);
+
     }
 }
 
