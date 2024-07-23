2c2962730475748fdd42da1c836ac31857237f65
Liubov <bavazapsha@gmail.com>
2024-07-23 17:44:16 +0300

    Created Task14.1

--- src/LiubovZapsa/Main.java
+++ src/LiubovZapsa/Main.java
@@ -1,18 +1,20 @@
 package LiubovZapsa;
 
-import java.util.ArrayList;
+import java.util.HashSet;
 
 public class Main {
 
     public static void main(String[] args) {
-        ArrayList<String> fruits = new ArrayList<>();
-        fruits.add("Apple");
-        fruits.add("Berry");
-        fruits.add("Watermelon");
-        fruits.add("Melon");
-        for (String fruit : fruits) {
-            System.out.println(fruit);
+        HashSet<String> names = new HashSet<String>();
+        names.add("Mark");
+        names.add("Jane");
+        names.add("Kris");
+        names.add("Tom");
+        names.size();
+        for (String name : names) {
+            System.out.println(name);
         }
+
     }
 }
 
