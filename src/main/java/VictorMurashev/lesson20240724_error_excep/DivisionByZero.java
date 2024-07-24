e47718ea92bace80a4a313e8bbfa19bd46b6289f
Victor Murashev <vmurashev@gmail.com>
2024-07-24 20:19:26 +0300

    VictorMurashev-lesson-20240724-null-pointer

--- src/main/java/VictorMurashev/lesson20240724_error_excep/DivisionByZero.java
+++ src/main/java/VictorMurashev/lesson20240724_error_excep/DivisionByZero.java
@@ -1,14 +1,21 @@
 package VictorMurashev.lesson20240724_error_excep;
 
 public class DivisionByZero {
+
     public static void main(String[] args) {
+        divide(10, 5);
+        divide(10, 0);
+    }
+
+    public static void divide(int a, int b) {
         try {
-            int a = 10;
-            int b = 0;
+            if (b == 0) {
+                throw new ArithmeticException("Division by zero is not allowed.");
+            }
             int result = a / b;
             System.out.println("The result is: " + result);
         } catch (ArithmeticException e) {
-            System.out.println("Error: Division by zero is not allowed.");
+            System.out.println("Error: " + e.getMessage());
         }
     }
 }
