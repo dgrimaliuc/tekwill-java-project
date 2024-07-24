e4be78ffad45b7a4b3d16d694721ccdcc74e54fe
Liubov <bavazapsha@gmail.com>
2024-07-24 22:23:18 +0300

    Created task21

---
+++ src/LiubovZapsa/Lesson170701/Person.java
@@ -0,0 +1,12 @@
+package LiubovZapsa.Lesson170701;
+
+public class Person {
+    public String fname = "Adam";
+    public String lname = "Smith";
+    public int age = 32;
+
+    public void printInfo() {
+        System.out.println("FirthName: " + fname + " " + "LastName:  " +
+                lname + " " + "Age: " + age);
+    }
+}
