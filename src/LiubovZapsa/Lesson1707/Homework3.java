cb6633569267e06705f3fd61d70689925b3c4c7a
Liubov <bavazapsha@gmail.com>
2024-07-24 16:28:38 +0300

    Created Task20

---
+++ src/LiubovZapsa/Lesson1707/Homework3.java
@@ -0,0 +1,16 @@
+package LiubovZapsa.Lesson1707;
+
+public class Homework3 {
+
+    public static void printNumbers(int x) {
+        for (int i = 1; i <= x; i++) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+
+        printNumbers(12);
+    }
+}
