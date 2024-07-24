0e49a64a0457762aff9fe22d120dbabf47a2ee81
Liubov <bavazapsha@gmail.com>
2024-07-24 15:54:46 +0300

    Created Task17

---
+++ src/LiubovZapsa/Lesson1707/Homework1.java
@@ -0,0 +1,27 @@
+package LiubovZapsa.Lesson1707;
+/*
+public class Homework1 {
+
+    static void greet(String name) {
+        System.out.println("Hello, " + name + "!");
+    }
+
+    public static void main(String[] args) {
+        greet("Liubov");
+        System.out.println();
+*/
+
+public class Homework1 {
+    static int getdifference(int x, int y) {
+        return x - y;
+    }
+
+    public static void main(String[] args) {
+        int num1 = 6;
+        int num2 = 4;
+        int difference = getdifference(num1, num2);
+        System.out.println("Difference: " + difference);
+    }
+}
+
+
