bbc2a53d36112bcadb0c2934ff48175ab2060de1
Liubov <bavazapsha@gmail.com>
2024-07-27 21:45:27 +0300

    Created task21

--- src/LiubovZapsa/Lesson170701/Book.java
+++ src/LiubovZapsa/Lesson170701/Book.java
@@ -5,6 +5,9 @@ public class Person {
     public String lname = "Smith";
     public int age = 32;
 
+    public Person(String liam, String brown, int i) {
+    }
+
     public void printInfo() {
         System.out.println("FirthName: " + fname + " " + "LastName:  " +
                 lname + " " + "Age: " + age);
