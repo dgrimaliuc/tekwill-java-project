02e51b892fe0020f49693c3b244f383c10a4fc37
Mironov Anastasia <natiamironova2709@gmail.com>
2024-07-27 21:29:46 +0300

    Homework ex3

---
+++ src/main/java/MironovAnastasia/Hw/ex3.java
@@ -0,0 +1,22 @@
+package IvanTrembaci.src.main.java.MironovAnastasia.Hw;
+
+import java.util.Scanner;
+
+public class ex3 {
+
+    /*
+      Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter your age");
+        int age = scanner.nextInt();
+        if (age >= 18){
+            System.out.println("You are of legal age");
+        }else{
+            System.out.println("You are not of legal age");
+        }
+     */
+
+    public static void main(String[] args) {
+
+
+    }
+}
