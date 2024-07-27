c043f0b50eadd4d8a20c4bdf9223761ad86dbcf5
Mironov Anastasia <natiamironova2709@gmail.com>
2024-07-27 21:02:46 +0300

    Homework ex1,2

---
+++ src/main/java/MironovAnastasia/Homework.java
@@ -0,0 +1,45 @@
+package IvanTrembaci.src.main.java.MironovAnastasia;
+
+public class Homework {
+    /*
+    1. Создайте две переменные и найдите их сумму (+):
+        int x = 25;
+        int y = 5;
+        int sum = x + y;
+        System.out.println("The sum of " + x + " and " + y + " is " +  sum);
+    }
+    1.1  Создайте две переменные и найдите их разность (-):
+        int x = 25;
+        int y = 5;
+        int difference = x - y;
+        System.out.println("The difference of " + x + " and " + y + " is " +  difference);
+    }
+    1.2  Создайте две переменные и найдите их произведение (*):
+        int x = 25;
+        int y = 5;
+        int product = x * y;
+        System.out.println("The product of " + x + " and " + y + " is " +  product);
+    }
+    1.3  Создайте две переменные и найдите их частное (/):
+        int x = 25;
+        int y = 5;
+        int quotient = x / y;
+        System.out.println("The quotient of " + x + " and " + y + " is " +  quotient);
+        }
+    2. Выведите на экран наименьшее из двух чисел:
+        int x = 25;
+        int y = 5;
+        if (x<y){
+         System.out.println("The smallest number is " + x);
+        } else if (x>y){
+         System.out.println("The smallest number is " + y);
+            {
+
+     */
+
+
+
+    public static void main(String[] args) {
+
+    }
+}
