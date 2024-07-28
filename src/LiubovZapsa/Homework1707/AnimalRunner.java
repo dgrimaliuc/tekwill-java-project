1b25d9fa117d34586bf5c5a1f7b518449b676b67
Liubov <bavazapsha@gmail.com>
2024-07-28 19:15:33 +0300

    Created Task24

---
+++ src/LiubovZapsa/Homework1707/AnimalRunner.java
@@ -0,0 +1,20 @@
+package LiubovZapsa.Homework1707;
+
+public class AnimalRunner {
+
+    public static void main(String[] args) {
+
+        Animal animal1 = new Animal("Murca", "cat", 5, "Meau-meau");
+        Animal animal2 = new Animal("Sharic", "dog", 3, "Gaf-gaf");
+        Animal animal3 = new Animal("Alenca", "cow", 6, "Muu-muu");
+
+        animal1.makeSound();
+        animal2.makeSound();
+        animal3.makeSound();
+        animal1.displayInfo();
+        animal2.displayInfo();
+        animal3.displayInfo();
+
+    }
+}
+
