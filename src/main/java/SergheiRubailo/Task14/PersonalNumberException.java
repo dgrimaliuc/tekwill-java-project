938b9e52e65cf73ac2184579298fd4d713348a02
serghei-rubailo <serghei.rubauilo@gmail.com>
2024-07-28 13:35:40 +0300

    SR-Task14

---
+++ src/main/java/SergheiRubailo/Task14/PersonalNumberException.java
@@ -0,0 +1,7 @@
+package IvanTrembaci.src.main.java.SergheiRubailo.Task14;
+
+public class PersonalNumberException extends NumberFormatException{
+    public PersonalNumberException(String s) {
+        super(s);
+    }
+}
