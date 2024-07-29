5c52eba81b2b5d7edc5e78d610c54aa311a7d990
Mihai <potoracmihai74@gmail.com>
2024-07-29 15:39:25 +0300

    PM-HomeWork_task8

---
+++ src/main/java/PotoracMihai/task8_24_07_2024/Exception/HomeWork_task8/PersonalNumberException.java
@@ -0,0 +1,7 @@
+package main.java.PotoracMihai.task8_24_07_2024.Exception.HomeWork_task8;
+
+public class PersonalNumberException extends RuntimeException {
+    public PersonalNumberException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/PotoracMihai/task8_24_07_2024/Exception/PersonalNullException.java
@@ -0,0 +1,4 @@
+package main.java.PotoracMihai.Exception;
+
+public class PersonalNullException {
+}
