bd6008bef619bd4d4211357e917b8f03306754ef
Mihai <potoracmihai74@gmail.com>
2024-07-29 15:49:17 +0300

    PM-lesson24_07_task8

--- src/main/java/PotoracMihai/task8_24_07_2024/Exception/PersonalBoundsException.java
+++ src/main/java/PotoracMihai/task8_24_07_2024/Exception/PersonalBoundsException.java
@@ -1,4 +1,12 @@
-package main.java.PotoracMihai.Exception;
+package main.java.PotoracMihai.task8_24_07_2024.Exception;
 
-public class PersonalBoundsException {
+/*
+Create an unchecked exception class PersonalNullException.
+Create a toUpperCase method and inside print the UpperCase of a provuded string.
+Throw a PersonalNullException when the provided string is null.
+ */
+public class PersonalBoundsException extends Exception {
+    public PersonalBoundsException(String message) {
+        super(message);
+    }
 }
--- src/main/java/PotoracMihai/task8_24_07_2024/Exception/PersonalNullException.java
+++ src/main/java/PotoracMihai/task8_24_07_2024/Exception/PersonalNullException.java
@@ -1,4 +1,7 @@
-package main.java.PotoracMihai.Exception;
+package main.java.PotoracMihai.task8_24_07_2024.Exception;
 
-public class PersonalNullException {
+public class PersonalNullException extends RuntimeException {
+    public PersonalNullException(String message) {
+        super(message);
+    }
 }
