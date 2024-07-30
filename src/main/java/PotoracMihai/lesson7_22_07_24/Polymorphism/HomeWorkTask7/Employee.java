fc64f830ac1573e26c526c604b1612cf8a332a86
Mihai <potoracmihai74@gmail.com>
2024-07-30 14:21:33 +0300

    PM-HomeWork_tasks7

---
+++ src/main/java/PotoracMihai/lesson7_22_07_24/Polymorphism/HomeWorkTask7/Employee.java
@@ -0,0 +1,5 @@
+package main.java.PotoracMihai.lesson7_22_07_24.Polymorphism.HomeWorkTask7;
+
+public abstract class Employee {
+    public abstract double calculateSalary();
+}
---
+++ src/main/java/PotoracMihai/lesson7_22_07_24/Polymorphism/HomeWorkTask7/FullTimeEmployee.java
@@ -0,0 +1,13 @@
+package main.java.PotoracMihai.lesson7_22_07_24.Polymorphism.HomeWorkTask7;
+
+public class FullTimeEmployee extends Employee {
+
+
+    public FullTimeEmployee() {
+    }
+
+    @Override
+    public double calculateSalary() {
+        return 1000;
+    }
+}
