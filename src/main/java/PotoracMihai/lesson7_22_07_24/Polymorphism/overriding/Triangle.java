1b699f0aef57c7619a53d1daca53ca3c7c3827b5
Mihai <potoracmihai74@gmail.com>
2024-07-30 14:25:08 +0300

    PM-Overriding_tasks7

---
+++ src/main/java/PotoracMihai/lesson7_22_07_24/Polymorphism/overriding/Triangle.java
@@ -0,0 +1,27 @@
+package main.java.PotoracMihai.lesson7_22_07_24.Polymorphism.overriding;
+
+import main.java.PotoracMihai.lesson7_22_07_24.Polymorphism.Interfaces.IShape;
+
+public class Triangle implements IShape {
+
+    private double side;
+    private double height;
+
+    public Triangle(double side, double height) {
+        this.side = side;
+        this.height = height;
+    }
+
+    @Override
+    public double calculateArea() {
+        return 0.5 * (side * height);
+    }
+
+    @Override
+    public String toString() {
+        return "Triangle{" +
+                "side=" + side +
+                ", height=" + height +
+                '}';
+    }
+}
