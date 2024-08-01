c450474906d7eecf32ff7b595081570841a31c45
Liubov <bavazapsha@gmail.com>
2024-08-01 14:18:32 +0300

    Created task11

---
+++ src/LiubovZapsa/Inheritance1/AnimalRunner.java
@@ -0,0 +1,9 @@
+package LiubovZapsa.Inheritance1;
+
+public class AnimalRunner {
+    public static void main(String[] args) {
+        Dog dog1 = new Dog("Mars", 4, "dog", "Gaf", "Blue", 10.2);
+        Dog dog2 = new Dog("Puf", 2, "dog", "Gaf", "Coli", 5.1);
+        dog1.printInfo();
+    }
+}
