f3a9a3cdac1bb1019cd6e4f23eb8c5161176e7cc
dgrimaliuc <116957079+dgrimaliuc@users.noreply.github.com>
2024-08-04 11:59:00 +0300

    Fixes (#293)
    Co-authored-by: denis.grimaliuc <denis.grimaliuc@crunchyroll.com>

--- src/LiubovZapsa/Lesson170701/Runner.java
+++
@@ -1,9 +0,0 @@
-package LiubovZapsa.Lesson170701;
-
-public class Runner {
-    public static void main(String[] args) {
-        Person person = new Person();
-        person.printInfo();
-
-    }
-}
