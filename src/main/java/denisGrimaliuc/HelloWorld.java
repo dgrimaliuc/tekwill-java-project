577faee3aa7250a09ea124ba000d8f29bb587edc
denis.grimaliuc <denis.grimaliuc@crunchyroll.com>
2024-12-11 19:47:53 +0200

    My First Class

--- src/main/java/denisGrimaliuc/HelloWorld.java
+++ src/main/java/denisGrimaliuc/HelloWorld.java
@@ -2,6 +2,8 @@ package main.java.denisGrimaliuc;
 
 public class HelloWorld {
     public static void  main (String[] args) {
+        // Commentariu
         System.out.println("Hello World");
+
     }
 }
