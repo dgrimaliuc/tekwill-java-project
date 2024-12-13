77c83767a64b2ae6e643688768a1d57df1a2af78
roliya <liliyarosca@gmail.com>
2024-12-13 18:44:26 +0200

    LR if else example

---
+++ src/main/java/LiliaRosca/if_else_ex.java
@@ -0,0 +1,12 @@
+package main.java.LiliaRosca;
+
+public class if_else_ex {
+    public static void main(String[] args) {
+        int age = 18;
+        if (age >= 18) { // <=
+            System.out.println("Esti adult.");
+        } else {
+            System.out.println("Esti minor.");
+        }
+    }
+}
