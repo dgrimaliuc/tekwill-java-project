a66349a5850172461a747b99b91a0d82cb0e8edc
Gsoft444 <g.diferite@gmail.com>
2024-12-13 19:35:06 +0200

    My First Class George

--- src/main/java/georgeVinaga/ClassWork_13_Dec_2024.java
+++ src/main/java/georgeVinaga/ClassWork_13_Dec_2024.java
@@ -2,11 +2,78 @@ package main.java.georgeVinaga;
 
 public class ClassWork_13_Dec_2024 {
     public static void main(String[] args) {
+//--------------------------------------------------------------------
+        /*
         int age = 18;
         if (age >= 18) {
             System.out.println("Ești adult.");
         } else {
             System.out.println("Ești minor.");
         }
+        */
+//--------------------------------------------------------------------
+        /*
+        int num = 10;
+        if (num > 0)
+            if (num % 2 == 0) {
+                System.out.println("Număr pozitiv par");
+            } else {
+                System.out.println("Număr pozitiv impar");
+            }
+        else if (num < 0) {
+            System.out.println("Număr negativ");
+        } else {
+            System.out.println("Zero");
+        }
+        */
+//--------------------------------------------------------------------
+        /*
+        // Găsește suma a două numere
+        int a = 2;
+        int b = 4;
+        int sum = a + b;
+        System.out.println(sum);
+        */
+//--------------------------------------------------------------------
+        /*
+        // Verifica daca un numar este pozitiv, negativ sau zero
+        int number = 0;
+        if (number > 0) {
+            System.out.println("Num is more then 0");
+        } else if (number < 0) {
+            System.out.println("Num is less than 0");
+        } else {
+            System.out.println("Num is 0");
+        }
+        */
+//--------------------------------------------------------------------
+        /*
+        // Verifica daca numaru este par sau impar
+        int number = 1;
+        if (number % 2 == 0) {
+            System.out.println("Num is par");
+        } else {
+            System.out.println("Num is impar");
+        }
+        */
+//--------------------------------------------------------------------
+        /*
+        // Verifică dacă un număr este divizibil cu 2 și 3.
+        int number = 1;
+        if (number % 2 == 0 && number % 3 == 0) {
+            System.out.println("Num is divisible by 2 and 3");
+        } else {
+            System.out.println("Num is not divisible by 2 and 3");
+        }
+        */
+//--------------------------------------------------------------------
+        // Citește un șir de caractere de la utilizator și afișează lungimea acestuia
+        /*
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a string: ");
+        String str = sc.nextLine();
+        System.out.println("String lenght is: " + str.length());
+         */
+//--------------------------------------------------------------------
     }
 }
