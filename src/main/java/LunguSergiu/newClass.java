767d8c569cc045cae0cc58381151d91a557504a0
pc1 <sergiudemo@gmail.com>
2024-12-13 16:49:08 +0200

    My Second Class

---
+++ src/main/java/LunguSergiu/newClass.java
@@ -0,0 +1,17 @@
+package main.java.LunguSergiu;
+
+public class newClass {
+    public static void main(String[] args) {
+        int myAge = 30;
+        int passportAge = 18;
+        System.out.println(myAge >= passportAge);
+        double pi = 3.14159265358979;
+        double elseNumber = 1.15e20;
+        System.out.println(pi == elseNumber);
+        String mesaj = "Bună seara";
+        System.out.println("Bună seara" + " colegi!");
+        boolean timpuldeafaraestebun = true;
+        boolean timpuldeafaraesterau = false;
+        System.out.println(timpuldeafaraestebun && timpuldeafaraesterau);
+    }
+}
