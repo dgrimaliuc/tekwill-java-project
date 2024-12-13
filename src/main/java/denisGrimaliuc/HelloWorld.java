19fabdb3b9305c674648547f40ad153399cec03a
Denis Grimaliuc <94832020+dgrimaliuc-crunchyroll@users.noreply.github.com>
2024-12-13 12:49:45 +0200

    DG Task1 (#298)
    * My First Class

---
+++ src/main/java/denisGrimaliuc/HelloWorld.java
@@ -0,0 +1,9 @@
+package main.java.denisGrimaliuc;
+
+public class HelloWorld {
+    public static void  main (String[] args) {
+        // Commentariu NR-2
+        System.out.println("Hello World");
+
+    }
+}
