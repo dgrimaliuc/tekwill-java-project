0756d30b2442c0358fd9629d50dcda97aa4a3ac4
Gsoft444 <g.diferite@gmail.com>
2024-12-13 18:35:25 +0200

    My First Class George

---
+++ src/main/java/georgeVinaga/ClassWork_13_Dec_2024.java
@@ -0,0 +1,12 @@
+package main.java.georgeVinaga;
+
+public class ClassWork_13_Dec_2024 {
+    public static void main(String[] args) {
+        int age = 18;
+        if (age >= 18) {
+            System.out.println("Ești adult.");
+        } else {
+            System.out.println("E;ti minor.");
+        }
+    }
+}
