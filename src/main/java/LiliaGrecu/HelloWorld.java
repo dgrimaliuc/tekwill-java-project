cc1c18b9f23ff36ca8c5edac08fec5b79f6b828e
lilia <lilia.grecu@outlook.com>
2024-12-13 19:05:57 +0200

    am creat o clasa

--- src/main/java/LiliaGrecu/HelloWorld.java
+++ src/main/java/LiliaGrecu/HelloWorld.java
@@ -2,11 +2,15 @@ package main.java.LiliaGrecu;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        int age = 18;
-        if (age >= 18) { // >=
-            System.out.println("Ești adult.");
+        int num = 10;
+        if (num > 0) {
+            if (num % 2 == 0) {
+                System.out.println("Număr pozitiv par");
+            } else {
+                System.out.println("Număr pozitiv impar");
+            }
+        } else if (num < 0) {
+            System.out.println("Număr negativ");
         } else {
-            System.out.println("Nu ești adult.");
+            System.out.println("Zero");
         }
-    }
-}
