90f1f61394ee7f374fe3f11261fdebebebaf3419
roliya <liliyarosca@gmail.com>
2024-12-18 18:43:11 +0200

    LR HW 16.12 not finished

---
+++ src/main/java/LiliaRosca/LR_old_clas1612.java
@@ -0,0 +1,35 @@
+package main.java.LiliaRosca;
+
+public class LR_old_clas1612 {
+    public static void main(String[] args) {
+        // HW 16.12
+        // 1. suma numerelor de la 55 la 111 folosind o buclă do-while
+        /*int a = 55;
+        int sum = 0;
+        do {
+            sum += a;
+            a++;
+        } while (a <= 111);
+        System.out.println("Suma numerelor de la 55 la 111 este de: " + sum);
+
+        // 2. suma numerelor de la 1 la 100 folosind o buclă for
+        int sum = 0;
+        for (int i = 1; i <= 100; i ++) {
+            sum += i;
+        }
+        System.out.println("Suma este de: " + sum);
+
+        // 3. găsește și afișează valoarea medie a tuturor elementelor din array de numere reale
+        double [] num = {1.15, 5.58, 8.01, 7.96, -84.84, 16.87, 584, 68, 78, 63};
+        double sum = 0;
+        int i = 0;
+        while (i < num.length) {
+            sum += num[i];
+            i++;
+        }
+        System.out.println("Media numere reale: " + sum / num.length);
+*/
+        // 4. afișează un array de tip String, definit în cadrul programului
+
+    }
+}
