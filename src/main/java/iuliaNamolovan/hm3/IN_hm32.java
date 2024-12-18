79fd719d1289f91b91ab06264626aeb9434ff5a7
IuliaNamolovan <berghiiiulia@yahoo.com>
2024-12-18 18:31:53 +0200

    IN-hm1

--- src/main/java/iuliaNamolovan/hm3/IN_hm32.java
+++ src/main/java/iuliaNamolovan/hm3/IN_hm32.java
@@ -3,12 +3,12 @@ package main.java.iuliaNamolovan.hm3;
 public class IN_hm32 {
     public static void main(String[] args) {
         //creaza un program care gaseste si afiseaza valoarea medie a tutror elementelor dintrun arrray de numere reale
-        int[] array = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-        int sum = 0;
-        for (int num : array) {
+        double[] ar = {1.1, 2.1, 3.1, 4.1, 5.1};
+        double sum = 0;
+        for (double num : ar) {
             sum += num;
         }
-        double med = (double) sum / array.length;
+        double med = (double) sum / ar.length;
         System.out.println("media este " + med);
     }
 }
--- src/main/java/iuliaNamolovan/hm3/IN_hm33.java
+++ src/main/java/iuliaNamolovan/hm3/IN_hm33.java
@@ -4,7 +4,10 @@ public class IN_hm33 {
     /// /creaza un program care afiseaza un array de tip string , definit in cadrul programului
     public static void main(String[] args) {
         String[] name = {"Ion", "vasile", "andrei", "vlad"};
-        System.out.println(name[3]);
-        System.out.println(name.length);
+        System.out.println("numele sunt");
+        for (String n : name) {
+            System.out.println(n + "");
+        }
+
     }
 }
