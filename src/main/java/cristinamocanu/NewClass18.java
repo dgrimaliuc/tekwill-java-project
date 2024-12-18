50c9554c324d0b3abf424b9ad8a641acc5cd9c90
cristinamocanu <cristin.mocanu@gmail.com>
2024-12-18 18:59:04 +0200

    CM 2 Class

---
+++ src/main/java/cristinamocanu/NewClass18.java
@@ -0,0 +1,18 @@
+package main.java.cristinamocanu;
+
+import java.util.ArrayList;
+
+public class NewClass18 {
+    public static void main(String[] args) {
+
+        ArrayList<String> cars = new ArrayList<String>();
+        cars.add("Volvo");
+        cars.add("BMW");
+        cars.add("Ford");
+        cars.add("Mazda");
+        for (String i : cars) {
+            System.out.println(i);
+        }
+
+
+    }   }
