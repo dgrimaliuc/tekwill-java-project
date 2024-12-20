862c3b419771a3c11aed8e88a6afe57a21d28187
IuliaNamolovan <berghiiiulia@yahoo.com>
2024-12-20 10:17:32 +0200

    IN-hm1

--- src/main/java/iuliaNamolovan/hm1/hm1.java
+++ src/main/java/iuliaNamolovan/hm1/hm1.java
@@ -1,9 +1,9 @@
-package main.java.iuliaNamolovan;
+package main.java.iuliaNamolovan.hm1;
 
 public class hm1 {
     public static void main(String[] args) {
-       int total_list = 20;
-       int present_list= 10;
+        int total_list = 20;
+        int present_list = 10;
         System.out.println(total_list - present_list);
     }
 }
