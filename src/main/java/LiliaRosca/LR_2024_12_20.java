7f1cac14062ca35e905f4132049f1e00db4170c4
roliya <liliyarosca@gmail.com>
2024-12-20 20:38:26 +0200

    LR 20 12

--- src/main/java/LiliaRosca/LR_2024_12_20.java
+++ src/main/java/LiliaRosca/LR_2024_12_20.java
@@ -1,31 +1,35 @@
 package main.java.LiliaRosca;
 
-public class LR_24_20_12_class_HW {
-    //2. Create a checkAge() method with an integer variable callde age
+public class LR_2024_12_20 {
+    //2. Create a checkAge() method with an integer variable called age
     /*static void checkAge(int age) {
         if (age < 18) {
             System.out.println("Access denied - You are not old enough!");
         } else {
             System.out.println("Access granted - You are old enough!");
         }
+    }
+    public static void main(String[] args) {
+        // 2. checkAge(20); // call the checkAge method
+    }
     // 3. Method that calculates average
-    public static double calcAver(int... numbers) {
+    public static double calculateAverage(int... numbers) {
       double sum = 0;
       for (int number :numbers) {
           sum += number;
       }
       return sum / numbers.length;
+    }
+    public static void main(String[] args) {
+        var average = calculateAverage( ... numbers 1, 2, 3, 4, 5, 6, 7, 121, 58);
+        System.out.println(average);
     }*/
     // 4. Scrie o metodă care primește doi întregi ca parametri și returnează diferența lor. (-)
     //Afiseaza difirenta in terminal.
-    public static int diference (int a, int b)
-}
-
-
+    public static int diference (int a, int b){
+    return a - b; // ceva nu e bine
+    }
     public static void main(String[] args) {
-        // 2. checkAge(20); // call the checkAge method
-        // 3.
-        /*var average = calcAver( ... numbers 1, 2, 3, 4, 5, 6, 7, 121,);
-        System.out.println(average);*/
+        System.out.println();
     }
 }
