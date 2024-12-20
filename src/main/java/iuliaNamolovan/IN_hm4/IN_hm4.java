b2986f954927717c0b7ec8b478de543692cdb788
IuliaNamolovan <berghiiiulia@yahoo.com>
2024-12-20 10:15:14 +0200

    IN-hm1

---
+++ src/main/java/iuliaNamolovan/IN_hm4/IN_hm4.java
@@ -0,0 +1,13 @@
+package main.java.iuliaNamolovan.IN_hm4;
+
+import java.util.ArrayList;
+
+public class IN_hm4 {
+    public static void main(String[] args) {
+        ArrayList<String> fructe = new ArrayList<>();
+        fructe.add("peach");
+        fructe.add("mango");
+        fructe.add("apple");
+        System.out.println("Fructele din lista sunt: " + fructe);
+    }
+}
