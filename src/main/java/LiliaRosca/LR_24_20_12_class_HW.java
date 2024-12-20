2ddcd457281e6ba173d7060e25b08a990f1e32c9
roliya <liliyarosca@gmail.com>
2024-12-20 19:29:12 +0200

    LR 20 12

--- src/main/java/LiliaRosca/LR_24_20_12_class_HW.java
+++ src/main/java/LiliaRosca/LR_24_20_12_class_HW.java
@@ -1,29 +1,31 @@
 package main.java.LiliaRosca;
 
-public class LR_2012_class_HW {
+public class LR_24_20_12_class_HW {
     //2. Create a checkAge() method with an integer variable callde age
     /*static void checkAge(int age) {
         if (age < 18) {
             System.out.println("Access denied - You are not old enough!");
         } else {
             System.out.println("Access granted - You are old enough!");
-        }    */
+        }
     // 3. Method that calculates average
-    public static double calculAverage(int... numbers) {
+    public static double calcAver(int... numbers) {
       double sum = 0;
       for (int number :numbers) {
           sum += number;
       }
       return sum / numbers.length;
-    }
-
-    // public static int suma
+    }*/
+    // 4. Scrie o metodă care primește doi întregi ca parametri și returnează diferența lor. (-)
+    //Afiseaza difirenta in terminal.
+    public static int diference (int a, int b)
 }
 
 
     public static void main(String[] args) {
         // 2. checkAge(20); // call the checkAge method
         // 3.
-        var average = calculAverage( ...numbers)
+        /*var average = calcAver( ... numbers 1, 2, 3, 4, 5, 6, 7, 121,);
+        System.out.println(average);*/
     }
 }
