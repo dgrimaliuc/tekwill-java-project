9067506ba244bb415fc480e73cf8e55eaf739c61
Denis Grimaliuc <94832020+dgrimaliuc-crunchyroll@users.noreply.github.com>
2025-01-08 18:23:35 +0200

    Ff (#344)

---
+++ src/main/java/denisGrimaliuc/incapsulation/Person.java
@@ -0,0 +1,36 @@
+package main.java.denisGrimaliuc.incapsulation;
+
+
+/**
+ * Creați o clasă numită „Person” cu variabile de instanță private pentru nume, vârstă și adresă.
+ * Includeți metode getter și setter corespunzătoare pentru fiecare variabilă.
+ */
+public class Person {
+    private String name;
+    private int age;
+    private String address;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
