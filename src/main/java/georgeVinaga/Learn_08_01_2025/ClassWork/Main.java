f9fabfc88e6c398df38302a262127abe73fc4670
Gsoft444 <g.diferite@gmail.com>
2025-01-10 17:12:18 +0200

    update 12 December files

--- src/main/java/georgeVinaga/Learn_08_01_2025/ClassWork/Main.java
+++ src/main/java/georgeVinaga/Learn_08_01_2025/ClassWork/Main.java
@@ -1,5 +1,10 @@
 package main.java.georgeVinaga.Learn_08_01_2025.ClassWork;
 
 public class Main {
+    public static void main(String[] args) {
+        Calculator calc = new Calculator();
+        double sum = calc.add(11.3,2.5);
+        System.out.println(sum);
+    }
 
 }
