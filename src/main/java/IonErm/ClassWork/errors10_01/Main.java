f549d2f6a8d4fdeb6af426625667a7ae6e2ccd7b
Ion Ermuratii <75805500+ErikGorin@users.noreply.github.com>
2025-01-13 12:23:01 +0200

    EGorin-Exceptions (#351)
    Co-authored-by: Ion <ion@debatezone.eu>

---
+++ src/main/java/IonErm/ClassWork/errors10_01/Main.java
@@ -0,0 +1,32 @@
+package main.java.IonErm.ClassWork.errors10_01;
+
+public class Main {
+
+    public static void toUpperCase(String upper) {
+        try {
+            System.out.println(upper.toUpperCase());
+        } catch (NullPointerException e) {
+            System.out.println("String is null");
+        }
+    }
+
+    public static void main(String[] args) {
+//        toUpperCase(null);
+
+//        try {
+//            throw new MyException("Custom exception occured!");
+//        } catch (MyException e) {
+//            System.out.println(e.getMessage());
+//        }
+//
+//        try {
+//            int[] numbers = {1, 2, 34, 5, 6, 7};
+//            System.out.println(numbers[1009]);
+//        } catch (Exception e) {
+//            System.out.println("The error is caught - something wrong");
+//        } finally {
+//            System.out.println("The Try Catch is finished");
+//        }
+
+    }
+}
