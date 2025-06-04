64b24f5403a89de5f822ea2b432e6b8bf905d6bc
nc <chihainatalia@gmail.com>
2025-06-04 20:37:13 +0300

    added if statement to get max value

--- src/main/java/NataliaChihai/Main.java
+++ src/main/java/NataliaChihai/Main.java
@@ -1,6 +1,31 @@
-package main.java.NataliaChihai;
-
 public class Main {
     public static void main(String[] args) {
+//        int num = 10;
+//        if (num>0){
+//            if (num%2 == 0){
+//                System.out.println("Numar pozitiv par");
+//            }
+//            else {
+//                System.out.println("Numar pozitiv impar");
+//            }
+//
+//        }else if (num<0){
+//            System.out.println("Numar negativ");
+//        } else {
+//            System.out.println("Zero");
+//        }
+        int max;
+        int a = 5;
+        int b = 6;
+        int c = 13;
+        if (a >= b && a >= c) {
+            max = a;
+        } else if (b >= a && b >= c) {
+            max = b;
+        } else {
+            max = c;
+        }
 
-    }}
+        System.out.println("Maximul este: " + max);
+    }
+}
