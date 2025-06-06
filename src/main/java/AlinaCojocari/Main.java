92d2714052a82af37bf628f9e065a1d960abd734
AlinaCojocari <alincik87@yahoo.com>
2025-06-06 19:36:32 +0300

    Homework: 1. Operatii cu variabile; 2. Verificare varsta

--- src/main/java/AlinaCojocari/Main.java
+++ src/main/java/AlinaCojocari/Main.java
@@ -1,6 +1,36 @@
 package main.java.AlinaCojocari;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        // 1. Operatii cu variabile
+        int a = 10;
+        int b = 4;
+        int suma = a + b;
+        int diferenta = a - b;
+        int produsul = a * b;
+        double catul = (double) a / b;
+        System.out.println("Suma: " + suma);
+        System.out.println("Diferenta: " + diferenta);
+        System.out.println("Produsul: " + produsul);
+        System.out.println("Catul: " + catul);
+        if (a < b) {
+            System.out.println("Cel mai mic numar este: " + a);
+        } else if (b < a) {
+            System.out.println("Cel mai mic numar este: " + b);
+        } else {
+            System.out.println("Numerele sunt egale");
+        }
+        // 2. Verificare varsta
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Introduceti varsta: ");
+        int varsta = scanner.nextInt();
+        if (varsta >= 18) {
+            System.out.println("Sunteti major.");
+        } else {
+            System.out.println("Nu suntei major.");
+        }
+    }
+}
 
-    }}
