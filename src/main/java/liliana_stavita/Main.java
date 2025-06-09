6f9049b2247496e96856f43a2e2b4cad95fa4ffb
LiliaSSS <lilililistv@gmail.com>
2025-06-09 19:31:00 +0300

    exercise

--- src/main/java/liliana_stavita/Main.java
+++ src/main/java/liliana_stavita/Main.java
@@ -1,4 +1,40 @@
 package main.java.liliana_stavita;
 
+
+import java.util.Scanner;
+
 public class Main {
+    // Verifică dacă un număr este pozitiv, negativ sau zero folosind if-else
+/*
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Introduceți un număr: ");
+        int numar = scanner.nextInt();
+
+        if (numar > 0) {
+            System.out.println("Numărul este pozitiv.");
+        } else if (numar < 0) {
+            System.out.println("Numărul este negativ.");
+        } else {
+            System.out.println("Numărul este zero.");
+        }
+    }
+}
+*/
+//
+//public static void main(String[] args) {
+//   int a, b, c;
+//   a = 1;
+//   b=2;
+//   c=3;
+//
+//   if (a > b && b > c) {
+//       System.out.println("First number is the biggest:" + a);
+//   } else if (b > a && b > c) {
+//       System.out.println("Second number is the biggest:" + b);
+//   }  else if (c > a && b > a) {
+//       System.out.println("Third number is the biggest:" + c);
+//   }
+//}
 }
