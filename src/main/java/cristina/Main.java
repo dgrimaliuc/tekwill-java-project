832363b0e9b7baaed17a858b25ae3487e3083b98
Cristina <cristina.corman@isa.utm.md>
2025-06-13 18:38:57 +0300

    Added a class Animal, Fruit, Student

--- src/main/java/cristina/Main.java
+++ src/main/java/cristina/Main.java
@@ -1,7 +1,17 @@
 package main.java.cristina;
 
+import java.util.ArrayList;
+import java.util.Objects;
+
 public class Main {
     public static void main(String[] args) {
+        ArrayList<Object> cars = new ArrayList<>();
+        cars.add("Volvo");
+        cars.add("BMV");
+        cars.add("Opel");
 
+        System.out.println("Lista completă: " + cars);
+
+        Object[] objs = {1, "12321", true};
     }
 }
