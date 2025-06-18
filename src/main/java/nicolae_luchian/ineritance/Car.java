3a18cedbae50d4755af7288d70709645ec0327a0
Nick Luchian <nicolai.luchian@gmail.com>
2025-06-18 18:49:38 +0300

    Inheritance practice

--- src/main/java/nicolae_luchian/ineritance/Car.java
+++ src/main/java/nicolae_luchian/ineritance/Car.java
@@ -7,7 +7,7 @@ public class Car extends Vehicle {
     public String fuelType;
 
     public Car() {
-        super(name, year, type);
+        super("vehicleName", 2000, "2000");
         this.fuelType = fuelType;
         this.numberOfDoors = numberOfDoors;
     }
